c467878b1cbf20520fa70df235a6824217a93701
Dan Finlay <dan@danfinlay.com>
2018-08-10 17:58:05 -0700

    Add time to soak function

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const waterPerSquareFootInch = 0.62 // gallons
 
 class InjectionCalculator {
 
+  /*
+   * Calculates the number of gallons required to soak an area of soil.
+   *
+   * @param {Number} area - The number of square feet of soil to soak.
+   * @param {Number} depth - The number of feet deep to inject water.
+   * @param {Number} fieldCapacity - The number of cubic inches of water that can be held by 1 foot deep of this soil. (Typically ranges from 1-1.5)
+   * @returns Number - gallons of water required
+   */
   waterNeeded ({ area = 0, depth = 3, fieldCapacity = 1 } = {}) {
     const soilVolumeToSoak = depth * area
     return soilVolumeToSoak * waterPerSquareFootInch * fieldCapacity
   }
 
+  /*
+   * The amount of time required for a given hose to emit a number of gallons.
+   * @returns {Number} minutes
+   */
+  timeToEmitGallons ({ gallons = 0, speed = 8 } = {}) {
+    return gallons / speed
+  }
+
+  timeToSoak ({ area = 0, depth = 3, fieldCapacity = 1, hoseSpeed = 8 } = {}) {
+    const waterNeeded = this.waterNeeded({ area, depth, fieldCapacity })
+    return this.timeToEmitGallons({
+      gallons: waterNeeded,
+      speed: hoseSpeed,
+    })
+  }
 }
 
 module.exports = InjectionCalculator
+
--- test/index.js
+++ test/index.js
@@ -29,5 +29,30 @@ test(`Test defaults`, (t) => {
   t.end()
 })
 
+test('hoseSpeed', (t) => {
+  const gallons = 100 // gallons
+  const speed = 10 // gallons per minute
 
+  const calc = new Calc()
+
+  const result = calc.timeToEmitGallons({ gallons, speed })
+  t.equal(result, 10, 'should take 10 minutes to soak')
+  t.end()
+})
+
+test('timeToSoak', (t) => {
+  const area = 100 // sq feet
+  const depth = 3 // feet
+  const fieldCapacity = 1 // inch (default)
+  const hoseSpeed = 10
+
+  const calc = new Calc()
+
+  const result = calc.timeToSoak({
+    area, depth, fieldCapacity, hoseSpeed
+  })
+
+  t.equal(result, 18.6, `got Molly's result`)
+  t.end()
+})
 
